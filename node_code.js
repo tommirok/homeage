be59eb27d852c0414d9b36cf8e48cab3623a4605
Tommi Rokolampi <tommi.rokolampi@hiq.fi>
2019-11-23 18:49:40 +0200

    trasparent iphone image and background

---
+++ node_code.js
@@ -0,0 +1,36 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function Request(args) {
+  const { uriEnd, val1 } = args;
+  this.apiURIStart = "http:/api/";
+  this.apiURIEnd = uriEnd;
+  this.val2 = 5;
+  const newValue = this.val2;
+  return (function api(){
+    return new Promise((resolve, reject) => {
+      let iszero = "";
+      iszero = val1 - newValue;
+  
+      console.log(iszero);
+      if (iszero === 0) {
+        resolve("reset succesfully");
+      } else if (iszero == 0 && iszero !== 0) {
+        reject(new Error("input must be type " instanceof Number));
+      } else {
+        reject(new Error("resetting failed"));
+      }
+    });
+  }())
+  
+}
+
+Request({ uriEnd: "someapi", val1: "5" })
+  .then(resp => {
+    console.log(resp);
+  })
+  .catch(err => {
+    console.warn(err);
+  });
